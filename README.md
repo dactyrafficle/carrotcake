7dfcd37dafb83498483d97b99f5a341f2d4e6634
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2021-02-27 20:16:00 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# ladybug
+plot points on a map via csv upload
+
+https://dactyrafficle.github.io/ladybug/
+
+Make sure that latitude is the first column, and latitude is the second column.
+
+Latitude goes from -90 to 90 degrees.
+The North Pole has a latitude of 90 degrees.
+The Equator has a latitude of 0 degrees.
+The South Pole has a latitude of -90 degrees.
+
+![](images/latitude.png)
+
+Longitude goes from -180 to 180 degrees.
+
+Hamilton, Ontario has coordinated 43.2557 N and 79.8711 W
+
+In this file I need to represent that as (43.2557,-79.8711) because the negative segment of Longitude, -180 <= x < 0 can be represented as |x| or Math.abs(x) or the absolute value of x West.
+
+
---
+++ carrotcake.js
@@ -0,0 +1,238 @@
+
+
+let data = fetch('carrotcake.csv?x=' + Math.random()).then(r => r.text()).then(d => {
+
+ //console.log(d);
+
+ let arr = csv_string_to_array_of_objects(d); // arr of objs
+ let my_geojson_obj = arr_of_objects_into_geojson_object(arr);
+ return my_geojson_obj;
+
+}).then(x => {
+  
+  console.log(x);
+  
+ // display the map
+ mapboxgl.accessToken = 'pk.eyJ1IjoicnRob21hc2lhbiIsImEiOiJjamY5NWt1MWIwZXBxMnFxb3N6NHphdHN3In0.p80Ttn1Zyoaqk-pXjMV8XA';
+ let map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/dark-v9', // other choices: light-v9; dark-v9; streets-v10
+  center: [-95.7129, 37.0902], // Hamilton, ON [-79.8711, 43.2557], US [-95.7129, 37.0902]
+  zoom: 3
+ });
+ 
+
+ map.on('load', function() {
+
+ 	map.addLayer({
+		id: 'locations',
+		type: 'circle',
+		source: {
+			type: 'geojson',
+			data: x  // the name of the array where the data is coming from
+		},
+		layout: {
+			visibility: 'visible'
+		},
+		paint: {
+			'circle-radius': {
+				'base': 4,
+				'stops': [[4, 4.25], [12, 14]] // circles get bigger between z3 and z14
+			},
+			'circle-color': '#FF6EC7',
+			'circle-opacity': 0.6
+		}
+	});
+
+
+ })
+
+
+  
+});
+
+
+
+
+
+
+
+// this function will add a layer [an array of geojson data] to the map
+function add_my_layer_to_the_map(my_geojson_obj) {
+	map.addLayer({
+		id: 'locations',
+		type: 'circle',
+		source: {
+			type: 'geojson',
+			data: my_geojson_obj  // the name of the array where the data is coming from
+		},
+		layout: {
+			visibility: 'visible'
+		},
+		paint: {
+			'circle-radius': {  // make a function of size of a certain variable
+				'base': 4,
+				'stops': [[4, 4.25], [12, 14]] // circles get bigger between z3 and z14
+			},
+			'circle-color': '#FF6EC7',
+			'circle-opacity': 0.6
+		}
+	});
+	
+	
+	map.addLayer({
+		id: 'textLabels',
+		type: 'symbol',
+		source: {
+			type: 'geojson',
+			data: my_geojson_obj  // the name of the array where the data is coming from
+		},
+		layout: {
+			//"text-field": ["get", "name1"],
+      "text-field": ["format",
+        ["get","name1"], {"font-scale":1.0}
+        //["get","name2"], {"font-scale":0.6}
+      ],
+			//"text-variable-anchor": ["top", "bottom", "left", "right"],
+			//"text-radial-offset": 0.5,
+			//"text-justify": "auto",
+			//"icon-image": ["concat", ["get", "icon"], "-15"]
+			//"text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+			"text-size": 20
+		},
+		paint: {
+			"text-color": "#ffccff"
+		}
+	});
+
+	// to add text
+	/*
+	map.addLayer({
+		id: 'labels',
+		type: 'circle',
+		source: {
+			type: 'geojson',
+			data: locations  // the name of the array where the data is coming from
+		},
+		layout: {
+			visibility: 'visible'
+		}
+	});
+	*/
+	//map.setLayoutProperty('locations', 'text-field', 'locations')
+	
+	/*
+	['format',
+	['get', 'ar'], { 'font-scale': 1.2 },
+	'\n', {},
+	['get', 'ar'], {
+	'font-scale': 0.8,
+	'text-font': ['literal', [ 'DIN Offc Pro Italic', 'Arial Unicode MS Regular' ]]
+	}
+	]);	
+	*/
+
+	
+};
+
+
+function csv_string_to_array_of_objects(csv_string) {
+  let data = csv_string.split(/\n/);
+  //let n_rows = data.length;
+  //console.log('n_rows: ' + n_rows);
+  
+  let headers = data[0].split(/,/);
+
+  for (let i = 0; i < headers.length; i++) {
+    
+    // SHOW THE WORD & THE CHAR CODES OF EACH LETTER
+    //console.log(headers[i] + ': ' + headers[i].length);
+    let b = [];
+    for (let k = 0; k < headers[i].length; k++) {
+      b.push(headers[i].charCodeAt(k));
+    }
+    //console.log(b);
+    
+    // DO A REPLACE TO GET RID OF BAD CHARACTERS
+    let str = headers[i].replace(String.fromCharCode(13),'');
+    headers[i] = str;
+    
+    // SHOW THE WORD & THE CHAR CODES OF EACH LETTER
+    let c = [];
+    for (let k = 0; k < headers[i].length; k++) {
+      c.push(headers[i].charCodeAt(k));
+    }
+    //console.log(c);
+    //console.log(headers[i] + ': ' + headers[i].length);
+    
+  }
+  
+  for (let i = 0; i < data.length; i++) {
+    data[i] = data[i].split(/,/); 
+  }
+  
+  let n_cols = data[0].length;
+  
+  let arr = [];
+  for (let y = 1; y < data.length; y++) {
+    let obj = {};
+    
+    for (let x = 0; x < n_cols; x++) {
+     obj[headers[x]] = data[y][x];
+    }
+    arr.push(obj);
+  }
+  
+  return arr;
+}
+
+
+
+function arr_of_objects_into_geojson_object(arr) {
+  let obj = {
+    'type': 'FeatureCollection',
+    'features': []
+  };
+ 
+// port,port_name,state,lat,lon,type_proc,tonnage
+ 
+	/* 
+	 it looks like a geojson file has 2 main objects
+	 1. type
+	 2. features, which is an array containing anonymous objects (each is a location) each with 3 sub-objects
+		 1. type
+		 2. geometry
+		 3. properties
+	*/
+
+  // start iterating from 1 bc data[0] is a header row
+  for (var i = 0; i < arr.length; i++) {
+    let a = {
+      'type': 'Feature',
+      'id': i,  // added this as part of step 13
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [
+          arr[i].lon,
+          arr[i].lat
+        ]
+      },
+      'properties': {
+        'rowId': i,
+        'port_id': arr[i].port_id,
+        'port_code': arr[i].port_code,
+        'port_name': arr[i].port_name,
+        'state': arr[i].state,
+        'type_proc': arr[i].type_proc,
+        'tonnage': arr[i].tonnage
+      }
+    }
+    // inside the for loop, push the object a into the obj object
+    obj.features.push(a);
+  }
+  return obj;
+}
+
+
+// show port codes and names once it gets small enough
+
